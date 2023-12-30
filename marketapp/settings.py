eddb2a2af0b23209d974d9bd419a2cb505d8e5e9
AswanthAnu <aswanthanu777@gmail.com>
2023-12-30 15:37:53 +0530

    Deploying to heroku

--- marketapp/settings.py
+++ marketapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Activate Django Heroku
+django_heroku.settings(locals())
